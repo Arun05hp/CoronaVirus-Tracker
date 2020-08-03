cc611a90a5268c76f1547747d6280a4d8505dfdd
Arun Kumar <kumar05.962013@gmail.com>
2020-08-03 19:00:48 +0530

    media query

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/app/Header";
+import MainBody from "./components/app/MainBody";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <MainBody />
     </div>
   );
 }
