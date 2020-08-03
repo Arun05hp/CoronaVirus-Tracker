7cd921794d104d20a4a246d2fa76d28c195a521b
Arun Kumar <kumar05.962013@gmail.com>
2020-08-03 18:24:59 +0530

    Corona Tracker

---
+++ src/components/app/Header.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "../../assets/css/header.css";
+
+import AppLogo from "../../assets/images/app_img.png";
+import MoonLogo from "../../assets/images/moon.svg";
+import SunLogo from "../../assets/images/sun.svg";
+const Header = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  function switchMode() {
+    const body = document.getElementById("body");
+    body.classList.toggle("dark");
+    setDarkMode((prevState) => !prevState);
+  }
+
+  return (
+    <header className="app_header">
+      <div className="container">
+        <div className="flexBox flex-csb">
+          <div className="app_title flexBox flex-c">
+            <div className="app_imgBox">
+              <img className="img-fluid" src={AppLogo} alt="Corona Virus" />
+            </div>
+            <span className="brand">Corona Tracker</span>
+          </div>
+          <div onClick={() => switchMode()} className="switch_icons">
+            <img
+              src="img-fluid"
+              src={darkMode ? SunLogo : MoonLogo}
+              alt="icons"
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
